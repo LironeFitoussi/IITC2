c074fa7535c894ea6023d602c7e8314b01333c83
LironeFitoussi <lironefit@gmail.com>
2024-07-25 15:46:10 +0300

    main uate for lessons order naming

---
+++ 23.07.2024/on class/main.js
@@ -0,0 +1,46 @@
+// Print Message to user console:
+console.log("Hello from main.js");
+
+// Print Message alert:
+// alert("Hello from main.js");
+
+
+// Variables:
+// let name = "Aviel";
+// let lastName = "Hajaj"
+
+// let age = 28;
+// let city = "Jerusalem"
+// const name1 = "Aviel";
+
+// age += 1
+// city -= ", Israel";
+
+// name = "Lirone"
+// name1 = "Lirone"
+// console.log(city);
+// console.log("Hello Aviel")
+// console.log("Godbye Aviel")
+// console.log(name)
+// console.log(age)
+// console.log('Hello my "name" is ' + name + ' ' + lastName + ', and my age is ' + age);
+
+// Bollean Data Type
+// console.log(5 < 6);
+
+// let isUserPremium = false;
+
+// // ....
+
+// isUserPremium = true;
+
+
+// Choises:
+let isUserActive = 1;
+
+if (isUserActive == true) {
+    console.log("Hello User!");
+} else {
+    console.log("Please Log in");
+}
+
